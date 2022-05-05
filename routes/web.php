969ae4825128b10c7c1bbf58610656771fcb1106
julian layen <julian@codecodex.fr>
2022-05-05 12:16:54 +0200

    04_04_d

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,11 @@ Route::get('/test', function () {
     return 'test';
 });
 
-// Default : retourner une vue
+// Default retourner une vue
 Route::get('/', function () {
     return view('welcome');
 });
  
-// Avec des paramètres
-Route::get('/bonjour/{name}', function ($name) {
-    return 'Nom : '.$name;
-});
-
-// Avec des paramètres non obligatoires
-Route::get('/user-mandatory/{name?}', function ($name = 'Julian') {
-    return $name;
-});
-
-// Avec une validation du type
-Route::get('/userbis/{name}', function ($name) {
-    return 'ici';
-})->where('name', '[A-Za-z]+');
-// ->where('id', '[0-9]+');
-
 // Get 
 // Route::get('/user', [UserController::class, 'index']);
 
@@ -63,6 +47,22 @@ Route::redirect('/test1', '/test', 301);
 // Listing des routes
 // php artisan route:list
 // php artisan route:list -v
+
+// Avec des paramètres
+Route::get('/bonjour/{name}', function ($name) {
+    return 'Nom : '.$name;
+});
+
+// Avec des paramètres non obligatoires
+Route::get('/user-mandatory/{name?}', function ($name = 'Julian') {
+    return $name;
+});
+
+// Avec une validation du type
+Route::get('/userbis/{name}', function ($name) {
+    return 'ici';
+})->where('name', '[A-Za-z]+');
+// ->where('id', '[0-9]+');
 
 // créer une route avec un nom
 Route::get('/user/profile', function () {
